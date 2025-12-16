8954b3fded99150cae2c0acb9145109039b5397e
Bobby Radford <bobby@rownd.io>
2025-12-16 16:55:43 -0500

    fix: add missing file extension in import

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-import RbmApiHelper from "./lib/rbm_api_helper";
+import RbmApiHelper from "./lib/rbm_api_helper.js";
 import { OAuth2Client, JWT } from "googleapis-common";
 export default RbmApiHelper;
 export { OAuth2Client, JWT };
